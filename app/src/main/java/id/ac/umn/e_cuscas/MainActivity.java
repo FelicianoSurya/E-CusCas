bc92ad888af99179cd32d434375fc8dce22b9c29
FelicianoSurya <suryamarcello@gmail.com>
2022-11-06 14:28:53 +0700

    Project E-CusCas

---
+++ app/src/main/java/id/ac/umn/e_cuscas/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.umn.e_cuscas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
