eb13f6f0d48883594a243d666c679d2012508b92
Deiyak <dea.noveriyanti@student.umn.ac.id>
2022-11-06 22:30:00 +0700

    login
    cuman nyooba ya ges yak

---
+++ app/src/main/java/id/ac/umn/e_cuscas/Login_page.java
@@ -0,0 +1,13 @@
+package id.ac.umn.e_cuscas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login_page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+}
