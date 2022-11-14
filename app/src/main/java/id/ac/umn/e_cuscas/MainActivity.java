c98f2bb4327e3bb39310b69793c70f9cd6806b66
FelicianoSurya <suryamarcello@gmail.com>
2022-11-14 19:24:27 +0700

    fix problem

--- app/src/main/java/id/ac/umn/e_cuscas/MainActivity.java
+++ app/src/main/java/id/ac/umn/e_cuscas/MainActivity.java
@@ -1,13 +1,11 @@
 package id.ac.umn.e_cuscas;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.app.FragmentManagerNonConfig;
-import android.content.Intent;
-import android.os.Bundle;
 
 import id.ac.umn.e_cuscas.databinding.ActivityMainBinding;
 
@@ -18,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
