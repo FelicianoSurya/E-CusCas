10fdeb68837a092ccc91944e0b6071575dee351e
keziaivena <kezia061201@gmail.com>
2022-12-08 11:57:36 +0700

    Login - Database

--- app/src/main/java/id/ac/umn/e_cuscas/Login_page.java
+++ app/src/main/java/id/ac/umn/e_cuscas/Login_page.java
@@ -3,6 +3,7 @@ package id.ac.umn.e_cuscas;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,12 +12,23 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import id.ac.umn.e_cuscas.model.User;
+import id.ac.umn.e_cuscas.remote.APIUtils;
+import id.ac.umn.e_cuscas.remote.UserService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Login_page extends AppCompatActivity {
 
     private EditText etUsername, etPassword;
     private Button btnLogin;
     private TextView warning, signUp;
     private String username, password;
+    private UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,7 @@ public class Login_page extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.btnRegister);
         warning = (TextView) findViewById(R.id.tvWarning);
         signUp = (TextView) findViewById(R.id.tvLogin);
+        userService = APIUtils.getUserService();
 
         warning.setText("");
 
@@ -39,18 +52,19 @@ public class Login_page extends AppCompatActivity {
                 password = etPassword.getText().toString();
 
                 if(!isNull(username) || !isNull(password)) {
-                    if(username.equals("admin")) {
-                        if(password.equals("admin")) {
-                            Intent goToNextActivity = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(goToNextActivity);
-                        } else {
-                            warning.setTextColor(Color.RED);
-                            warning.setText("Incorrect Password!");
-                        }
-                    } else {
-                        warning.setTextColor(Color.RED);
-                        warning.setText("Incorrect Username!");
-                    }
+                    reqLogin(username, password);
+//                    if(username.equals("admin")) {
+//                        if(password.equals("admin")) {
+//                            Intent goToNextActivity = new Intent(getApplicationContext(), MainActivity.class);
+//                            startActivity(goToNextActivity);
+//                        } else {
+//                            warning.setTextColor(Color.RED);
+//                            warning.setText("Incorrect Password!");
+//                        }
+//                    } else {
+//                        warning.setTextColor(Color.RED);
+//                        warning.setText("Incorrect Username!");
+//                    }
                 } else {
                     warning.setTextColor(Color.RED);
                     warning.setText("Username and Password cannot be empty!");
@@ -81,5 +95,24 @@ public class Login_page extends AppCompatActivity {
 
     boolean isNull(String string){
         return string == null || string.isEmpty();
+    }
+
+    private void reqLogin(String username, String password){
+        Call<User> call = userService.loginUser(username, password);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if(response.isSuccessful()){
+                    Intent goToNextActivity = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(goToNextActivity);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                warning.setTextColor(Color.RED);
+                warning.setText("Wrong Username or Password!");
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/umn/e_cuscas/remote/UserService.java
+++ app/src/main/java/id/ac/umn/e_cuscas/remote/UserService.java
@@ -4,18 +4,22 @@ import id.ac.umn.e_cuscas.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface UserService {
-    @GET("api/posts/")
-    Call<User> getUsers();
+    @POST("v1/login")
+    Call<User> loginUser(
+            @Field("username") String username,
+            @Field("password") String password
+    );
     @POST("v1/register")
     Call<User> addUser(@Body User user);
-    @PUT("api/posts/{id}")
-    Call<User> updateUser(@Path("id") int id, @Body User user);
-    @DELETE("api/posts/{id}")
-    Call<User> deleteUser(@Path("id") int id);
+    //@PUT("api/posts/{id}")
+    //Call<User> updateUser(@Path("id") int id, @Body User user);
+    //@DELETE("api/posts/{id}")
+    //Call<User> deleteUser(@Path("id") int id);
 }
