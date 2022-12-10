f7e9d0d682edac89c0e56ae5e817ee0abd36cb3a
keziaivena <kezia061201@gmail.com>
2022-12-10 17:32:51 +0700

    Login - Database (update)

--- app/src/main/java/id/ac/umn/e_cuscas/remote/UserService.java
+++ app/src/main/java/id/ac/umn/e_cuscas/remote/UserService.java
@@ -5,12 +5,14 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface UserService {
+    @FormUrlEncoded
     @POST("v1/login")
     Call<User> loginUser(
             @Field("username") String username,
