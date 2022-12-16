26e4213b25226e8f3eea04fbf6ba40d6a2413232
keziaivena <kezia061201@gmail.com>
2022-12-16 20:09:29 +0700

    Category - Database (update)

--- app/src/main/java/id/ac/umn/e_cuscas/remote/JSONResponse.java
+++ app/src/main/java/id/ac/umn/e_cuscas/remote/JSONResponse.java
@@ -3,9 +3,9 @@ package id.ac.umn.e_cuscas.remote;
 import id.ac.umn.e_cuscas.model.Category;
 
 public class JSONResponse {
-    private Category[] data;
+    private Category[] dataCategory;
 
-    public Category[] getData() {
-        return data;
+    public Category[] getDataCategory() {
+        return dataCategory;
     }
 }
