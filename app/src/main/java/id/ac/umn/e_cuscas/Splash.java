87f283e78acdc7758cc4abc8c85ef7b39e579778
Deiyak <dea.noveriyanti@student.umn.ac.id>
2022-12-18 22:26:35 +0700

    Splash screen

---
+++ app/src/main/java/id/ac/umn/e_cuscas/Splash.java
@@ -0,0 +1,28 @@
+package id.ac.umn.e_cuscas;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login_page.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+        }
+}
